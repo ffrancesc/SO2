35f314438f58e051bfa48a7fb6bf36604058b380
francesc <96salarfrancesc@gmail.com>
2021-03-07 15:13:15 +0100

    Fixes directory mapping on container

--- Lab/docker-up.sh
+++ Lab/docker-up.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 docker build --rm -t so2-lab .
 
+xhost + ${hostname}
+
 docker run \
     --rm -it \
     --name SO2-Lab \
     -e DISPLAY=host.docker.internal:0 \
-    so2-lab
-    #-v ${PWD}/zeos:/home/zeos \
+    -v ${PWD}/../..:/workspaces \
+    so2-lab "/bin/bash"
