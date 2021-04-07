bb05f18c0b451775581bb024ad9453f7c08e12c8
francesc <96salarfrancesc@gmail.com>
2021-04-07 03:48:34 +0200

    fork (misses proper handling of memory)

--- Lab/zeos/sys.c
+++ Lab/zeos/sys.c
@@ -20,6 +20,8 @@
 #define ESCRIPTURA 1
 
 extern int zeos_ticks;
+int ret_from_fork(void);
+int next_pid = 10;
 
 int check_fd(int fd, int permissions)
 {
@@ -40,11 +42,44 @@ int sys_getpid()
 
 int sys_fork()
 {
-  int PID=-1;
+    int PID=-1;
+    
+    // a) create the child process
+    if (list_empty(&freequeue)) return -ENOMEM; // No free process left.
+    struct list_head *e = list_first(&freequeue); 
+    struct task_struct *ts_child = list_head_to_task_struct(e);
+    union task_union *tu_child = (union task_union *) ts_child;
+    list_del(e);
 
-  // creates the child process
-  
-  return PID;
+    // b) copy parent's 
+    struct task_struct *ts_parent = current();
+    union task_union *tu_parent  = (union task_union *) ts_parent;
+    copy_data(ts_parent, ts_child, sizeof(union task_union));
+
+    // c) initialize dir_pages_baseAddr with a new directory
+    allocate_DIR(ts_child);
+
+    // TODO: page shenanigans
+    // d)
+    // e)
+
+
+    // f) assign PID to the process.
+    PID = next_pid++;
+    ts_child->PID = PID;
+
+    // g) 
+    set_quantum(ts_child, 1000);
+
+    // h)
+    tu_child->stack[1023] = ret_from_fork;
+    tu_child->stack[1022] = 0;
+
+    // i)
+    list_add_tail(&ts_child->list, &readyqueue);
+
+    // j)
+    return PID;
 }
 
 void sys_exit()
